4887d5ac282daa5b62392bca5490c14826443676
xiatao123 <xiatao123@hotmail.com>
2014-01-12 21:58:24 -0800

    Initial Commit

---
+++ TempConv/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TempConv
+//
+//  Created by Tao Xia on 1/12/14.
+//  Copyright (c) 2014 Tao Xia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ TempConvTests/TempConvTests.m
@@ -0,0 +1,34 @@
+//
+//  TempConvTests.m
+//  TempConvTests
+//
+//  Created by Tao Xia on 1/12/14.
+//  Copyright (c) 2014 Tao Xia. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TempConvTests : XCTestCase
+
+@end
+
+@implementation TempConvTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
